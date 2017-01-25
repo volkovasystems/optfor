43c1106b3c777807c7fc134311441237cbbd9c36
volkovasystems <richeve.bebedor@gmail.com>
2017-01-25 12:22:45 +0800

    Updated dependency.

---
+++ .install.sh
@@ -0,0 +1,25 @@
+yarn add \
+babel-runtime \
+doubt \
+falze \
+harden \
+optall \
+protype \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+doubt \
+falze \
+harden \
+optall \
+protype \
+zelf \
+--save
