5300d5c91d50a0efc70c4d7355d088b0b8149a48
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-13 20:01:45 +0800

    Updated server/client test.

--- optfor-test.js
+++ optfor-test.js
@@ -1,6 +1,12 @@
 const assert = require( "assert" );
 const optfor = require( "./optfor.js" );
 
-assert.equal( optfor( [ 1, 2, 3 ], 2, true ), 2 );
+assert.equal( optfor( [ 1, 2, 3 ], 2, true ), 2, "should return 2" );
+
+assert.equal( optfor( [ 1, 2, 3, "hello" ], "hello" ), "hello", "should return 2" );
+
+assert.equal( optfor( [ 1, 2, 3, true ], true ), true, "should return true" );
+
+assert.equal( optfor( [ 1, 2, 3, Symbol.for( "hello" ) ] , SYMBOL ), Symbol.for( "hello" ), "should be equal" );
 
 console.log( "ok" );
