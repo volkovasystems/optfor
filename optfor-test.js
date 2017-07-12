1171f84b1e51efa1ffe3b1ba16bdd642f717ca37
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-12 14:23:08 +0800

    Updated server/client test.

--- optfor-test.js
+++ optfor-test.js
@@ -9,4 +9,11 @@ assert.equal( optfor( [ 1, 2, 3, true ], true ), true, "should return true" );
 
 assert.equal( optfor( [ 1, 2, 3, Symbol.for( "hello" ) ] , SYMBOL ), Symbol.for( "hello" ), "should be equal" );
 
+assert.deepEqual( optfor( [ 1, 2, 3, /eah/ ], RegExp ), /eah/, "should be equal" );
+
+assert.equal( optfor( [ 1, 2, 3, "hello" ], STRING ), "hello", "should return 'hello'" );
+
+let test = function hello( ){ return "hello" };
+assert.deepEqual( optfor( [ null, Symbol.for( "hi" ), test ], FUNCTION ), test, "should be deeply equal" );
+
 console.log( "ok" );
